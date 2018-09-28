d1979dacfc466eeefe1113de73c11b065fe14de0
Zsolt Nagy <zsolt.nagy@jaystack.com>
2018-09-28 11:29:40 +0200

    added socket io to store

---
+++ frontend/src/constans.js
@@ -0,0 +1,2 @@
+export const PUBLIC_API = "http://hackathon.guidesmiths.com:4000/";
+export const SOCKET_IO = ""
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,11 @@
 import Store, { thunk } from 'repatch';
+import socketIOClient from 'socket.io-client'
+import { SOCKET_IO } from "./constans"
+
+export const socket = socketIOClient(SOCKET_IO);
+
 
 export default new Store({
   me: '',
   gameState: null
-}).addMiddleware(thunk.withExtraArgument({}));
+}).addMiddleware(thunk.withExtraArgument({socket}));
