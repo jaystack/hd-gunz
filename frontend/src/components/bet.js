d72c73a4e92352ebc2665210c6fbe8c037abccff
Zsolt Nagy <zsolt.nagy@jaystack.com>
2018-09-28 13:18:30 +0200

    added some stuff to bet

--- frontend/src/components/bet.js
+++ frontend/src/components/bet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import bullet from '../assets/bullet.png'
 import '../css/revolver.css'
+import '../css/button.css'
 import char1 from "../assets/char1.png";
 import char2 from "../assets/char2.png";
 import char3 from "../assets/char3.png";
@@ -17,12 +18,20 @@ class bet extends Component {
         bet : 0, 
         current : 100,
         bullet: 3,
-        numberOfPlayers : 4
+        numberOfPlayers : 4,
+        betReady: false
     }
 
     takeBet = () => { 
-
+        this.setState({betReady : !this.state.betReady})
     }
+
+    attempBet = () => {
+        if(this.state.betReady) return;
+        this.setState({bet : 10 , current : this.state.current - 10})
+    }
+
+
     render() {
         return (
             <div>
@@ -34,14 +43,23 @@ class bet extends Component {
                 </div>
                 <div className="coinContainer">
                     <div className="coin gold"><p>{this.state.current}</p></div>
-                    <div className="coin silver"><p>{this.state.bet}</p></div>
+                    <div onClick={this.attempBet} className="coin silver"><p>{this.state.bet}</p></div>
                 </div> 
-                <div className="revolverContainer">
-                    <div class="loader">
-                        <div class="inner">
+                {!this.state.betReady && (
+                    <div className="buttonContainer">
+                        <a onClick={this.takeBet} href="#" class="myButton">Bet</a>
                     </div>
+                )
+                }
+                    {this.state.betReady && (
+                    <div className="revolverContainer">
+                        <div class="loader">
+                            <div class="inner">
+                        </div>
+                        </div>
                     </div>
-                </div>
+                    )
+                }
             </div>
         );
     }
