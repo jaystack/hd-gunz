c89a004b609a4d42ee1ae6111a0579ebc5930af6
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-09-28 13:34:28 +0200

    wip

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -6,3 +6,15 @@ export const register = username => state => (dispatch, getState, { socket }) =>
 export const ready = () => state => (dispatch, getState, { socket }) => {
   socket.emit('ready');
 };
+
+export const bet = amount => state => (dispatch, getState, { socket }) => {
+  socket.emit('bet', { amount });
+};
+
+export const betSubmit = () => state => (dispatch, getState, { socket }) => {
+  socket.emit('betSubmit');
+};
+
+export const shoot = () => state => (dispatch, getState, { socket }) => {
+  socket.emit('shoot');
+};
--- frontend/src/components/bet.js
+++ frontend/src/components/bet.js
@@ -1,68 +1,63 @@
 import React, { Component } from 'react';
-import bullet from '../assets/bullet.png'
-import '../css/revolver.css'
-import '../css/button.css'
-import char1 from "../assets/char1.png";
-import char2 from "../assets/char2.png";
-import char3 from "../assets/char3.png";
-import char4 from "../assets/char4.png";
+import { connect } from 'react-redux';
+import bullet from '../assets/bullet.png';
+import '../css/revolver.css';
+import '../css/button.css';
+import char1 from '../assets/char1.png';
+import char2 from '../assets/char2.png';
+import char3 from '../assets/char3.png';
+import char4 from '../assets/char4.png';
 
-const playersAssets = [
-    char1,char2,char3,char4
-]
+const playersAssets = [char1, char2, char3, char4];
 
+export default connect(state => {
+  const me = state.gameState.players.find(p => p.username === state.me);
+  return {
+    usernames: state.gameState.players.map(p => p.username),
+    bullets: me ? me.bullets : 0,
+    budget: me ? me.budget : 0,
+    bet: me ? me.bet : 0,
+    submitted: me ? me.betSubmitted : false
+  };
+})(
+  class bet extends Component {
+    takeBet = () => {
+      this.props.bet(5);
+    };
 
-class bet extends Component {
-
-    state = { 
-        bet : 0, 
-        current : 100,
-        bullet: 3,
-        numberOfPlayers : 4,
-        betReady: false
-    }
-
-    takeBet = () => { 
-        this.setState({betReady : !this.state.betReady})
-    }
-
-    attempBet = () => {
-        if(this.state.betReady) return;
-        this.setState({bet : 10 , current : this.state.current - 10})
-    }
-
+    attempBet = () => {
+      if (this.props.betSubmitted) return;
+      this.props.betSubmit();
+    };
 
     render() {
-        return (
-            <div>
-                <div className="bulletContainer">
-                    {[...Array(this.state.bullet)].map((e, i) => <img src={bullet} key={i} className="bullet"/>)}
-                </div>
-                <div className="playersContainer">
-                     {[...Array(this.state.numberOfPlayers)].map((e, i) => <img src={playersAssets[i]} key={i} className="chars"/>)}
-                </div>
-                <div className="coinContainer">
-                    <div className="coin gold"><p>{this.state.current}</p></div>
-                    <div onClick={this.attempBet} className="coin silver"><p>{this.state.bet}</p></div>
-                </div> 
-                {!this.state.betReady && (
-                    <div className="buttonContainer">
-                        <a onClick={this.takeBet} href="#" className="myButton">Bet</a>
-                    </div>
-                )
-                }
-                    {this.state.betReady && (
-                    <div className="revolverContainer">
-                        <div className="loader">
-                            <div className="inner">
-                        </div>
-                        </div>
-                    </div>
-                    )
-                }
-            </div>
-        );
+      const { usernames, bullets, budget, bet, betSubmitted } = this.props;
+      return (
+        <div>
+          <div className="bulletContainer">
+            {Array.from({ length: bullets.length }).map((e, i) => <img src={bullet} key={i} className="bullet" />)}
+          </div>
+          <div className="playersContainer">
+            {Array.from({ length: usernames.length }).map((e, i) => (
+              <img src={playersAssets[i]} key={i} className="chars" />
+            ))}
+          </div>
+          <div className="coinContainer">
+            <div className="coin gold"><p>{budget}</p></div>
+            <div onClick={this.attempBet} className="coin silver"><p>{bet}</p></div>
+          </div>
+          {!betSubmitted &&
+            <div className="buttonContainer">
+              <a onClick={this.takeBet} href="#" className="myButton">Bet</a>
+            </div>}
+          {betSubmitted &&
+            <div className="revolverContainer">
+              <div className="loader">
+                <div className="inner" />
+              </div>
+            </div>}
+        </div>
+      );
     }
-}
-
-export default bet;
+  }
+);
