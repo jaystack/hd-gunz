1a10060c0d8276adbc36ba6f7027f2df8f277970
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-09-28 13:37:47 +0200

    wip

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -1,5 +1,6 @@
 export const register = username => state => (dispatch, getState, { socket }) => {
   socket.emit('register', { username });
+  dispatch(ready());
   dispatch(state => ({ ...state, me: username }));
 };
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import io from 'socket.io-client';
 export const socket = io('http://localhost:5000');
 socket.on('connect', () => {
   console.log('connect');
+  socket.emit('reset');
   socket.on('change', gameState => {
     console.log('change', gameState);
     store.dispatch(state => ({ ...state, gameState }));
