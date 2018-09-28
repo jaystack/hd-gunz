7f0c0c0c70b458d1792454697a13a2369c783490
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2018-09-28 15:49:19 +0200

    wip

--- frontend/src/components/bet.js
+++ frontend/src/components/bet.js
@@ -15,7 +15,7 @@ export default connect(
   state => {
     const me = state.gameState.players.find(p => p.username === state.me);
     return {
-      usernames: state.gameState.players,
+      players: state.gameState.players,
       bullets: me ? me.bullets : 0,
       budget: me ? me.budget : 0,
       bet: me ? me.bet : 0,
@@ -44,33 +44,33 @@ export default connect(
     };
 
     render() {
-      const { usernames, bullets, budget, bet, betSubmitted, me } = this.props;
+      const { players, bullets, budget, bet, betSubmitted, me } = this.props;
       return (
         <div>
           <div className="bulletContainer">
             {Array.from({ length: bullets }).map((e, i) => <img src={bullet} key={i} className="bullet" />)}
           </div>
           <div className="playersContainer">
-            {usernames.map(
-              (e, i) =>
-                (e.username !== me.username
+            {players.map(
+              (p, i) =>
+                (me && p.username !== me.username
                   ? <div key={i} className="userContainer">
                       <img src={playersAssets[i]} className="chars" />
                       <div className="userDetials">
-                        <p> {e.username}</p>
-                        <p>Bet : {e.bet}</p>
+                        <p> {p.username}</p>
+                        <p>Bet : {p.bet}</p>
                       </div>
                     </div>
                   : null)
             )}
           </div>
-          <div className="coinContainer">
+          <div className="coinContainer" onClick={this.takeBet}>
             <div onClick={this.attempBet} className="coin gold"><p>{budget}</p></div>
             <div className="coin silver"><p>{bet}</p></div>
           </div>
           {!betSubmitted &&
             <div className="buttonContainer">
-              <a onClick={this.takeBet} href="#" className="myButton">Start</a>
+              <a href="#" className="myButton">Start</a>
             </div>}
           {betSubmitted &&
             <div className="revolverContainer">
